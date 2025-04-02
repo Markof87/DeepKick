998fbee9a588243136e00c1eacdd46e02d944478
fabianim <m.fabiani@ns12.it>
2025-04-02 15:00:21 +0200

    Fatta pulizia file

--- whoscored/dao/match_scraper_dao.py
+++ whoscored/dao/match_scraper_dao.py
@@ -6,7 +6,7 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-from shared.utils import createEventsDF, fetch_top_tournaments
+from shared.utils import fetch_top_tournaments
 
 from config import WHOSCORED_URL_BASE
 
